6bf081de968fcd6321e23f41f0119ce95b137ca9
yuzuki <66053193+yuzuki01@users.noreply.github.com>
2024-06-29 14:10:50 +0800

    readme update

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
 @echo off
 
 :main
-
 cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" -S "./" -B "./cmake-build-windows"
 cmake --build "./cmake-build-windows" --target clean
 cmake --build "./cmake-build-windows" --target "meso-mpi" -- -j 8
