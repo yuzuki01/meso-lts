6e261c9aa60e1163a9afb8dc337cabc4e270f95e
yuzuki01 <1226352404@qq.com>
2024-09-20 16:54:45 +0800

    para.py update

--- files/scripts/para.py
+++ files/scripts/para.py
@@ -14,14 +14,11 @@ d = 4.17e-10
 # Gas specific gas constant
 R = k / m
 # Characteristic length, m
-L = 20.0e-06
+L = 4.0e-7
 # Knudsen number
-Kn = 0.72
+Kn = 0.1
 # Characteristic temperature, K
 T = 273.0
-# dimensionless viscosity
-muBar = 5.0 * (alpha + 1.0) * (alpha + 2.0) * sqrt(pi) * Kn \
-        / (4.0 * alpha * (5.0 - 2 * omega) * (7.0 - 2.0 * omega))
 # mean free path, m
 mfp = L * Kn
 # initial density field, Kg/m^3
@@ -29,13 +26,15 @@ rho = m / (mfp * sqrt(2.0) * pi * d * d)
 # Most probable speed of molecular, m/2
 C = sqrt(2 * R * T)
 # viscosity
-mu = rho * C * L * muBar
-# Adiabatic constant
-g = 1.4
+mu = (15 * rho * mfp) / (2 * (5 - 2 * omega) * (7 - 2 * omega)) \
+     * sqrt(2 * pi * R * T)
+# adiabatic constant
+g = (5 + k) / (3 + k)
 
-print("Refer Temperature  (T )  = ", T)
-print("Knudsen number     (Kn)  = ", Kn)
-print("Viscosity          (mu)  = ", mu)
-print("Density           (rho)  = ", rho)
-print("Spefic gas constant (R)  = ", R)
-print("Most probable speed (C)  = ", C)
+print("Refer Density      (rho)  = ", rho)
+print("Refer Length         (L)  = ", L)
+print("Refer Temperature    (T)  = ", T)
+print("Knudsen number      (Kn)  = ", Kn)
+print("Viscosity           (mu)  = ", mu)
+print("Spefic gas constant  (R)  = ", R)
+print("Most probable speed  (C)  = ", C)
